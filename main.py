888d07cb10620784afc4c02a449f9a7a5273ebe5
vvertazu <uakbarhonov5@gmail.com>
2025-11-14 22:10:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiogram.filters import Command, Text
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import KeyboardButton
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
